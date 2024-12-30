22d8d759c23f636fa36c88b3b28c0a2cdd4ac200
KibiBC <69991794+Lv2Kode2Pwr@users.noreply.github.com>
2024-12-30 17:00:50 +0300

    Add files via upload

---
+++ src/components/Results.vue
@@ -0,0 +1,32 @@
+<template>
+  <p>time: {{ scoree }}</p>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+    props: ['scoree'],
+    data() {
+        return {
+            rank: null
+        }
+    }, 
+    mounted() {
+        if (this.scoree < 450) {
+            this.rank = 'Ninja Fingers'
+        } else if (this.scoree < 800) {
+            this.rank = 'Rapid Reflexes'
+        } else {
+            this.rank = 'Snail pace...'
+        }
+    }
+}
+</script>
+
+<style>
+.rank {
+    font-size: 1.4em;
+    color: #0faf87;
+    font-weight: bold;
+}
+</style>
